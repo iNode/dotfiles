c19d5707783ec353332e52c84bd7daee76464602
Stan. S. Krupoderov <pashelper@gmail.com>
2011-12-19 16:05:04 +0400

    update makeln.sh script to support scripts

--- makeln.sh
+++ makeln.sh
@@ -4,9 +4,13 @@ dir=`dirname $0`
 cd $dir
 dir=`pwd`
 
-for f in `ls -1a ./ | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh)"`; do
-#    echo $f
+for f in `ls -1a ./ | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
     ln -sf $dir/$f $HOME
+done
+
+mkdir -p $HOME/bin
+for f in `ls -1a ./scripts | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
+    ln -sf $dir/scripts/$f $HOME/bin
 done
 
 
