e939633661a0ce656a0451712cddb64097b91473
Stan. S. Krupoderov <pashelper@gmail.com>
2013-08-15 22:57:28 +0400

    add some podcast recording routines

---
+++ shell.d/podcast.sh
@@ -0,0 +1,40 @@
+
+prec() {
+    title="stub"
+    if [ ! -z $1 ]; then
+        title="$1"
+    fi
+    # echo $title
+    \cd ~/podcasts/
+    # find last record
+    # find . -name "${title}_???\.*" | wc -l
+    last=$(find . -name "${title}_???\.*" | \
+        perl -pe 's/.*'$title'_(\d+)\..*/$1/' | \
+        sort -n | tail -n 1 )
+    # echo $last
+    [ -z $last ] && last=0
+    echo $(( ++last ))
+    filename=$(printf "%s_%04d.wav" $title $last)
+    # echo $filename
+    arecord -vv -c 1 -f S16_LE -r24000 $filename
+}
+
+pplay() {
+    title="stub"
+    if [ ! -z $1 ]; then
+        title="$1"
+    fi
+    # echo $title
+    \cd ~/podcasts/
+    # find last record
+    # find . -name "${title}_???\.*" | wc -l
+    last=$(find . -name "${title}_???\.*" | \
+        perl -pe 's/.*'$title'_(\d+)\..*/$1/' | \
+        sort -n | tail -n 1 )
+    # echo $last
+    [ -z $last ] && last=1
+    # echo $(( ++last ))
+    filename=$(printf "%s_%04d.wav" $title $last)
+    # echo $filename
+    aplay $filename
+}
