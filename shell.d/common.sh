f0c2c702aec2611226f64561d044ea5d798e3455
Stan. S. Krupoderov <pashelper@gmail.com>
2013-08-15 22:55:02 +0400

    show some key bindings by key_help

---
+++ shell.d/common.sh
@@ -0,0 +1,12 @@
+key_help () {
+    echo "Ctrl-A           Start of line"
+    echo "Ctrl-E           End of line"
+    echo "Alt-B            Previous word"
+    echo "Alt-F            Next word"
+    echo "Ctrl-X Ctrl-X    Start or end of line"
+    echo "Ctrl-K           Delete rest of line"
+    echo "Alt-D            Delete next word"
+    echo "Ctrl-W           Delete previous word"
+    echo "Ctrl-X Ctrl-E    Open command in $EDITOR"
+    echo "^wrong^right^    Replace word in previous command"
+}
