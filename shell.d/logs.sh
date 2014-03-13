f026447edcc2cf136fd8dab980ba0a660473274d
Stan. S. Krupoderov <pashelper@gmail.com>
2014-03-13 06:13:11 +0000

    alias to show critical messages from dmesg

--- shell.d/logs.sh
+++ shell.d/logs.sh
@@ -8,3 +8,6 @@ fi
 alias slog='S tail -f /var/log/syslog | $colorizer'
 alias mlog='S tail -f /var/log/maillog | $colorizer'
 alias dlog='S tail -f /var/log/daemon.log | $colorizer'
+
+# critical dmesg
+alias cdmesg="dmesg -xT -l err,crit,emerg"
