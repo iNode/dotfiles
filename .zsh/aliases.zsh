75d7dc511a1ab1a0a6151a8a1792c3ebf1a73e7d
Stan. S. Krupoderov <pashelper@gmail.com>
2014-03-15 15:42:50 +0400

    add zsh specific aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -16,4 +16,22 @@ compctl -g *(/) -- cd_ls
 alias signfile="gpg -u iNode --clearsign -b -v "
 alias encrypt="gpg -c -v "
 
+# zsh specific aliases
+alias -g L=less
+# shell.d function
+alias -g T=stail
+alias -g G="grep -rnH "
+
+# extensions
+alias -s ps=evince
+alias -s pdf=evince
+alias -s doc=libreoffice
+alias -s docx=libreoffice
+alias -s xls=libreoffice
+alias -s xlsx=libreoffice
+alias -s html=sensible-browser
+alias -s htm=sensible-browser
+alias -s avi=vlc
+alias -s mp4=vlc
+
 # vim: set noet ts=4 tw=80 syntax=zsh
