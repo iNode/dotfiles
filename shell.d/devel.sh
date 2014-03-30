2ae6b4bed8b4854bd6a201cb5e210ff95e6d95bd
Stan. S. Krupoderov <pashelper@gmail.com>
2014-03-30 15:06:45 +0400

    fix ffip function

--- shell.d/devel.sh
+++ shell.d/devel.sh
@@ -3,10 +3,10 @@ function ffip()
     # find file in project
     # when call without arg returns root project path
     # root of project determined by .git/.hg/etc. folder
-    pattern="-d .git -o -d .hg -d .projectroot"
+    pattern="-d .git -o -d .hg -o -d .projectroot"
     curdir=`pwd`
     while [ "x`pwd`" != "x/" ]; do
-        if [ $pattern ]
+        if [ `echo $pattern` ]
         then
             if [ "x$1" != "x" ];
             then
@@ -51,12 +51,15 @@ function mgrep ()
   eval $cmd
 }
 
+function cdr ()
+{
+    cd $(ffip)
+}
 
 # print stack trace of a core file without needing to enter gdb interactively
 alias gdbbt='gdb -q -n -ex bt -batch'
 alias gdbr='gdb --args'
 
-alias cdr="cd `ffip`"
 alias gn="grep -rnH"
 
 alias xmlfmt="tr '\n' ' ' | xmllint --format - |pygmentize -l xml"
