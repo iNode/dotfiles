17b0cf6efe67e456cb899cea4103fe0bdcc340f3
Stan. S. Krupoderov <pashelper@gmail.com>
2014-03-30 15:14:29 +0400

    allow directories to have self install scripts

--- makeln.sh
+++ makeln.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
+startdir=`pwd`
 dir=`dirname $0`
 cd $dir
 dir=`pwd`
 
-for f in `ls -1a ./ | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
-    ln -sf $dir/$f $HOME
+my_pattern="(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"
+
+# use directory specific install scripts when available
+for d in $(find $dir -type d | grep -v -e .git -e scripts -e .$); do
+    if [ -x $d/install ]; then
+        # self install
+        $d/install
+    else
+        # just symlink
+        ln -sf $d $HOME
+    fi
 done
 
+# symliks for files
+for f in `find $dir -maxdepth 1 -type f | egrep -v $my_pattern`; do
+    ln -sf $f $HOME
+done
+
+# special care about scripts dir
 mkdir -p $HOME/bin
 for f in `ls -1a ./scripts | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
     ln -sf $dir/scripts/$f $HOME/bin
 done
 
 
+cd $startdir
