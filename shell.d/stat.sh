c9c2a6d69750aa39298f24a4198d9149e86a50b1
Stan. S. Krupoderov <pashelper@gmail.com>
2015-08-12 22:44:39 +0300

    fix functions declaration

--- shell.d/stat.sh
+++ shell.d/stat.sh
@@ -1,5 +1,5 @@
 # some statistical commands
 
 # zip directory exclude temporarily files
-comtop { history -1000 | awk '{print $2}' | sort | uniq -c | sort -rn | head; }
-pkgtop { dpkg-query -W -f='${Installed-Size} ${Package} \n' | sort -n; }
+comtop() { history -1000 | awk '{print $2}' | sort | uniq -c | sort -rn | head; }
+pkgtop() { dpkg-query -W -f='${Installed-Size} ${Package} \n' | sort -n; }
