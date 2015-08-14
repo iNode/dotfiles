2242321d3ea301b0582c5ef12a68429af976d6ec
Stan. S. Krupoderov <pashelper@gmail.com>
2015-08-14 16:18:42 +0000

    add dry-run mode

--- makeln.sh
+++ makeln.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 startdir=`pwd`
 dir=`dirname $0`
@@ -6,27 +6,40 @@ cd $dir
 dir=`pwd`
 
 my_pattern="(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"
+if [ $# -eq 1 -a "$1" == "-d" ]; then
+    dry_run=1
+else
+    dry_run=0
+fi
+
+run_cmd() {
+    if [ "$dry_run" == "1" ]; then
+        echo "$@"
+    else
+        $@
+    fi
+}
 
 # use directory specific install scripts when available
 for d in $(find $dir -type d | grep -v -e .git -e scripts -e .$); do
     if [ -x $d/install ]; then
         # self install
-        $d/install
+        run_cmd $d/install
     else
         # just symlink
-        ln -sf $d $HOME
+        run_cmd ln -sf $d $HOME
     fi
 done
 
 # symliks for files
 for f in `find $dir -maxdepth 1 -type f | egrep -v $my_pattern`; do
-    ln -sf $f $HOME
+    run_cmd ln -sf $f $HOME
 done
 
 # special care about scripts dir
 mkdir -p $HOME/bin
 for f in `ls -1a ./scripts | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
-    ln -sf $dir/scripts/$f $HOME/bin
+    run_cmd ln -sf $dir/scripts/$f $HOME/bin
 done
 
 
