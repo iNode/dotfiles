dc9a6923b6cbf24815d25c3801f4880f37de937f
Stan. S. Krupoderov <pashelper@gmail.com>
2016-07-23 15:42:04 +0300

    use apt insead of aptitude when available, nocorrect for acs

--- shell.d/debian.sh
+++ shell.d/debian.sh
@@ -2,9 +2,21 @@
 if [ -f /etc/debian_version ];
 then
     alias mydeb='time dpkg-buildpackage -rfakeroot -us -uc'
-    alias sai='nocorrect sudo aptitude install '
-    alias eis='nocorrect aptitude search '	# only in program names
-    alias acs='apt-cache search '	# also in descriptions
-    alias aps='aptitude show  '	# show info about a package
+    if apt -h 2>/dev/null 1>/dev/null ; then
+        alias eis='nocorrect apt list '	# only in program names
+        alias aps='nocorrect apt show  '	# show info about a package
+        alias sai='nocorrect sudo apt install '
+        alias eisi='nocorrect apt list --installed '
+        alias eisu='nocorrect apt list --upgradeable '
+        eisd () { aptitude search \~d$* }
+    else # apt not available yet
+        alias eis='nocorrect aptitude search '	# only in program names
+        alias aps='nocorrect  aptitude show  '	# show info about a package
+        alias sai='nocorrect sudo aptitude install '
+        eisi () { aptitude search \~i$* }
+        eisd () { aptitude search \~d$* }
+    fi
+
+    alias acs='nocorrect apt-cache search '	# also in descriptions
     alias dq='noglob dpkg-query '
 fi
