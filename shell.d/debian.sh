d96fc969dee6fef20dbcd2afff963468141fc58e
Stan. S. Krupoderov <pashelper@gmail.com>
2016-09-26 13:20:41 +0300

    use aptitude aliase only when available, update apt aliases

--- shell.d/debian.sh
+++ shell.d/debian.sh
@@ -2,19 +2,23 @@
 if [ -f /etc/debian_version ];
 then
     alias mydeb='time dpkg-buildpackage -rfakeroot -us -uc'
-    if apt -h 2>/dev/null 1>/dev/null ; then
-        alias eis='nocorrect apt list '	# only in program names
-        alias aps='nocorrect apt show  '	# show info about a package
-        alias sai='nocorrect sudo apt install '
-        alias eisi='nocorrect apt list --installed '
-        alias eisu='nocorrect apt list --upgradeable '
-        eisd () { aptitude search \~d$* }
-    else # apt not available yet
+    if apttiude -h 2>/dev/null 1>/dev/null ; then
+        # apt not available yet
         alias eis='nocorrect aptitude search '	# only in program names
         alias aps='nocorrect  aptitude show  '	# show info about a package
         alias sai='nocorrect sudo aptitude install '
-        eisi () { aptitude search \~i$* }
-        eisd () { aptitude search \~d$* }
+        function eisi () { aptitude search \~i$* ; }
+        function eisd () { aptitude search \~d$* ; }
+    else
+        if apt -h 2>/dev/null 1>/dev/null ; then
+            # alias eis='nocorrect apt list '	# only in program names
+            alias aps='nocorrect apt show  '	# show info about a package
+            alias sai='nocorrect sudo apt install '
+            alias eisi='nocorrect noglob apt list --installed '
+            alias eisu='nocorrect noglob apt list --upgradeable '
+            alias eis='nocorrect noglob apt search' # only in program names
+            #eisd () { aptitude search \~d$* }
+        fi
     fi
 
     alias acs='nocorrect apt-cache search '	# also in descriptions
