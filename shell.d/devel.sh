547b15a75aba909d6791c7208ba8599c536134ee
Stan. S. Krupoderov <pashelper@gmail.com>
2017-07-24 07:36:08 +0300

    fix minor syntax issues

--- shell.d/devel.sh
+++ shell.d/devel.sh
@@ -4,11 +4,11 @@ ffip()
     # when call without arg returns root project path
     # root of project determined by ROOT_PROJECT env varable or .git/.hg/.projectroot/etc. folder
     if [ -n "$PROJECT_ROOT" ]; then
-        cd $PROJECT_ROOT
+        cd "$PROJECT_ROOT"
         if [ "x$1" != "x" ];
         then
             find "$(pwd)" -iname "$@"
-            break
+            return
         fi
         pwd
         return
@@ -16,7 +16,7 @@ ffip()
     pattern="-d .git -o -d .hg -o -d .projectroot -o -f Cargo.toml -o -f README.md -o -d .idea -o -d .projectile"
     curdir=$(pwd)
     while [ "x$(pwd)" != "x/" ]; do
-        if [ $pattern ]; then
+        if [ "$pattern" ]; then
             if [ "x$1" != "x" ]; then
                 find "$(pwd)" -iname "$@"
             else
@@ -65,10 +65,10 @@ cdr ()
 }
 
 # ag + less function
-agl () { ag --color "$@" | less -R }
+agl () { ag --color "$@" | less -R; }
 
 # sift + less function
-siftl () { sift --color "$@" | less -R }
+siftl () { sift --color "$@" | less -R; }
 
 # cat to file and make it executable
 scat () { cat > "$1"; chmod +x "$1"; }
@@ -84,7 +84,7 @@ alias xmlfmt="tr -d '\n' | xmllint --format - |pygmentize -l xml"
 alias gb='go install -v'
 
 wait_success() {
-    cmd="$@"
+    cmd="$*"
     $cmd;
     while [ "x$?" != "x0" ]; do
 	sleep 1;
@@ -103,8 +103,8 @@ else
 fi
 
 
-svndiff () { svn diff "${@}" | colordiff | less -R -E }
-hgdiff () { hg diff "${@}" | colordiff | less -R -E }
+svndiff () { svn diff "${@}" | colordiff | less -R -E; }
+hgdiff () { hg diff "${@}" | colordiff | less -R -E; }
 
 rdiff() {
     if [ $# -lt 3 ]; then
@@ -169,12 +169,12 @@ p4co () {
     done
 }
 
-p4shelve() { p4 shelve "$@"; p4 revert "$@" }
+p4shelve() { p4 shelve "$@"; p4 revert "$@"; }
 
 
 grep_nascii() {
     # grep files with non ascii characters
     # ag "[\x80-\xFF]" "$@"
     # grep --color='auto' -R -P -n '[^\x00-\x7F]' ../../
-    grep --color='auto' -P -n '[^\x00-\xFF]' "$@"
+    grep --color='auto' -P -n '[^\x00-\xFF]' "$@" ;
 }
