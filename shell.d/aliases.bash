752e93aa6c1c1dec9daceeb5b07d9ff3ee14db2f
Stan. S. Krupoderov <pashelper@gmail.com>
2017-07-25 07:00:00 +0300

    minor syntax fix

--- shell.d/aliases.bash
+++ shell.d/aliases.bash
@@ -2,6 +2,15 @@
 alias signfile="gpg -u iNode --clearsign -b -v "
 alias encrypt="gpg -c -v "
 
+if which gtts_import 1>/dev/null; then
+    # echo "create say function"
+    say() {
+	. ~/pyenv3/bin/activate
+	gtts_import "$@" || gtts_import "$@"
+	deactivate
+    }
+fi
+
 # Local Variables:
 # mode: sh
 # End:
