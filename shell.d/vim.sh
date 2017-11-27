dccbd1b312e6dbecf35014fe3c9de308f2807eb6
Stan. S. Krupoderov <pashelper@gmail.com>
2017-11-27 21:12:27 +0300

    add vim related aliases, minor cleanup

--- shell.d/vim.sh
+++ shell.d/vim.sh
@@ -1,16 +1,17 @@
 # vim specific
-if which vim >/dev/null 2>&1;
-then
+if which vim >/dev/null 2>&1; then
    alias mutt="EDITOR=vim mutt"
    alias vi=vim
    alias revim="vim -S ~/.vim/lastSession.vim"
    # vim server mode
-   alias v='vim --servername $(hostname) $@'
-   alias gvim='gvim --servername $(hostname) $@'
+   alias v='vim --servername $(hostname)'
+   alias gvim='gvim --servername $(hostname)'
    # foreground edit
-   alias fv='vim --servername $(hostname) --remote-wait "$@"'
+   alias fv='vim --servername $(hostname) --remote-wait'
    # background edit in server
-   alias ev='vim --servername $(hostname) --remote-silent $@'
+   alias ev='vim --servername $(hostname) --remote-silent'
+   # review current maps
+   alias vmap='vim -c ":map" -c ":q" | less -R'
 else
     alias v='$EDITOR'
 fi
