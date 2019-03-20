4f2e81f6d5625d3946c7985a80e5b5edaeb4916e
Stanislav Krupoderov <pashelper@gmail.com>
2019-03-20 22:07:53 +0300

    add python specific aliases and functions

--- shell.d/python.sh
+++ shell.d/python.sh
@@ -2,3 +2,15 @@
 # python easy-install shorcut
 alias e-i='easy_install -d ~/.python/lib -s ~/.python/bin'
 
+# For arbitrary, non-venvwrapper-managed envs
+enter() {
+    venv_dir="$1"
+    [ -n "$venv_dir" ] && [ -d "$venv_dir" ] || return
+    # shellcheck disable=SC1090
+    . "${venv_dir}/bin/activate";
+}
+
+# environment variables
+# prevent global package installation
+export PIP_REQUIRE_VIRTUALENV=true
+alias gpip='PIP_REQUIRE_VIRTUALENV="" pip'
