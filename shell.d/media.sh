4c06a11e02c462579b0c0bf3a30253b1617e47b5
Stanislav Krupoderov <pashelper@gmail.com>
2019-07-17 00:37:04 +0300

    add some media functions: encode in ogg, open camera in vlc

--- shell.d/media.sh
+++ shell.d/media.sh
@@ -3,3 +3,16 @@
 alias mocs='mocp -Q "%state %title"'
 # manage usb headphone
 alias usbmixer="alsamixer -D hw:1"
+
+# open vlc with stream from camera
+function cam-open {
+  cvlc -f v4l2:///dev/video0
+}
+
+# encode sound track from source file to ogg format
+do-ogg() {
+    wavout=out.wav
+    oggfile=out.ogg
+    nice mplayer "$1" -ao pcm:file="$wavout" fast 1>/dev/null 2>&1
+    nice oggenc - -b 256 >! "$oggfile" < "$wavout"
+}
