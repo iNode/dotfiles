6c4c0b0b24af8c18dd0a951c7aac55cae6838600
Stanislav Krupoderov <pashelper@gmail.com>
2019-08-18 21:08:02 +0300

    fix patterns and shellcheck warnings

--- makeln.sh
+++ makeln.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-startdir=`pwd`
-dir=`dirname $0`
-cd $dir
-dir=`pwd`
+startdir=$(pwd)
+dir=$(dirname "$0")
+cd "$dir" || exit 1
+dir=$(pwd)
 
-my_pattern="(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts|init.sh|packages)"
-if [ $# -eq 1 -a "$1" == "-d" ]; then
+my_pattern='(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts|init.sh|packages|dotfiles/$|^config$)'
+if [ $# -eq 1 ] && [ "$1" == "-d" ]; then
     dry_run=1
 else
     dry_run=0
@@ -16,31 +16,31 @@ run_cmd() {
     if [ "$dry_run" == "1" ]; then
         echo "$@"
     else
-        $@
+        "$@"
     fi
 }
 
 # use directory specific install scripts when available
-for d in $(find $dir -maxdepth 1 -type d | grep -v -e .git -e scripts -e '\.$'); do
-    if [ -x $d/install ]; then
+for d in $(find "$dir" -maxdepth 1 -type d | grep -v -e '.git$' -e scripts -e "$dir\$" -e '\.$'); do
+    if [ -x "$d/install" ]; then
         # self install
-        run_cmd $d/install
+        run_cmd "$d/install"
     else
         # just symlink
-        run_cmd ln -sf $d $HOME
+        run_cmd ln -svf "$d" "$HOME"
     fi
 done
 
 # symliks for files
-for f in `find $dir -maxdepth 1 -type f | egrep -v $my_pattern`; do
-    run_cmd ln -sf $f $HOME
+for f in $(find "$dir" -maxdepth 1 -type f | grep -vE "$my_pattern"); do
+    run_cmd ln -svf "$f" "$HOME"
 done
 
 # special care about scripts dir
-mkdir -p $HOME/bin
-for f in `ls -1a ./scripts | egrep -v "(^\.git$|.swp|\.\/$|\.$|makeln.sh|scripts)"`; do
-    run_cmd ln -sf $dir/scripts/$f $HOME/bin
+mkdir -pv "$HOME/bin"
+for f in $(echo scripts/* | grep -vE '(^\.git$|.swp|\.\/$|\.$|makeln.sh|^scripts$)'); do
+    run_cmd ln -sf "$dir/$f" "$HOME/bin"
 done
 
 
-cd $startdir
+cd "$startdir" || exit 1
