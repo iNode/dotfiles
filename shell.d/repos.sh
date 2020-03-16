369cd8320b6b222dad259d6b949182c2f68a1ceb
Stanislav Krupoderov <pashelper@gmail.com>
2020-03-17 00:12:13 +0300

    add less support

--- shell.d/repos.sh
+++ shell.d/repos.sh
@@ -2,6 +2,6 @@
 
 # repos aliases
 #======
-alias repu='cd ~/repos/maint/; nice ./update all 2>&1 | tee last_update.log | ccze'
-alias repul='cd ~/repos/maint/; cat last_update.log - | ccze'
+alias repu='cd ~/repos/maint/; nice ./update all 2>&1 | tee -a last_update.log | ccze -A | less -R'
+alias repul='cd ~/repos/maint/; cat last_update.log - | ccze -A | less -R'
 # end repos
