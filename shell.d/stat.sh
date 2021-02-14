fcd40ccdca0a137c52c41932a3446be00df3873b
Stanislav Krupoderov <pashelper@gmail.com>
2021-02-14 15:20:01 +0300

    add pkgtop and comtop functions

--- shell.d/stat.sh
+++ shell.d/stat.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 # some statistical commands
 
-# zip directory exclude temporarily files
+# top commands from history
 comtop() { history -1000 | awk '{print $2}' | sort | uniq -c | sort -rn | head; }
+# top packages by size
 pkgtop() { dpkg-query -W -f='${Installed-Size} ${Package} \n' | sort -n; }
